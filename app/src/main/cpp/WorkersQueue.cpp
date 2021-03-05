1680a6de40759a381b16a80ccdcbb959e787db45
Dramcryx <jopa19991@gmail.com>
2021-03-05 18:58:43 +0300

    Add runners variant 4 and 5. 5 is stabilized 4

--- app/src/main/cpp/WorkersQueue.cpp
+++ app/src/main/cpp/WorkersQueue.cpp
@@ -16,7 +16,7 @@ wrappers::WorkersQueue::WorkersQueue(std::size_t workers)
 {
     mWorkers.reserve(workers);
     for (int i = 0; i < mWorkers.capacity(); ++i) {
-        mWorkers.emplace_back(&WorkersQueue::run3, this);
+        mWorkers.emplace_back(&WorkersQueue::run5, this);
     }
 
 
@@ -92,6 +92,7 @@ void wrappers::WorkersQueue::addToQueue(wrappers::WorkersQueue::TaskContext &&bu
 {
     std::lock_guard<std::mutex> lockGuard(mQueueProtector);
     mTasks.push_back(buffer);
+    Logger::logError(32, "QUEUE SIZE: %d", mTasks.size());
 }
 
 void wrappers::WorkersQueue::run()
@@ -339,6 +340,7 @@ void wrappers::WorkersQueue::run2()
 
 void wrappers::WorkersQueue::run3()
 {
+    // STABLE BUT SLOW
     // RUN2 BUT PARALLEL CONVERT AND ROTATION
     AHardwareBuffer_Desc desc{};
     desc.width = 1920;
@@ -474,6 +476,347 @@ void wrappers::WorkersQueue::run3()
                 auto out = (uint8_t *) surfaceBuffer.bits;
 
                 libyuv::ARGBCopy(y, surfaceBuffer.stride * 4, out, surfaceBuffer.stride * 4,
+                                 surfaceBuffer.width, surfaceBuffer.height);
+
+                auto redraw_end = std::chrono::high_resolution_clock::now();
+
+                isSurfaceLockFailed = ANativeWindow_unlockAndPost(task.surface);
+                currentFrame = task.frameNumber;
+                surfaceUsed = false;
+                Logger::logError(100, "TIME TO SCALE-DOWN: %d, FRAME %d",
+                                 std::chrono::duration_cast<std::chrono::milliseconds>(
+                                         scale_end - scale_start).count(),
+                                 currentFrame.load(std::memory_order_relaxed));
+                Logger::logError(100, "TIME TO MAKE ARGB: %d, FRAME %d",
+                                 std::chrono::duration_cast<std::chrono::milliseconds>(
+                                         argb_end - argb_start).count(),
+                                 currentFrame.load(std::memory_order_relaxed));
+                Logger::logError(100, "TIME TO ROTATE: %d, FRAME %d",
+                                 std::chrono::duration_cast<std::chrono::milliseconds>(
+                                         rotate_end - rotate_start).count(),
+                                 currentFrame.load(std::memory_order_relaxed));
+                Logger::logError(100, "TIME TO REDRAW: %d, FRAME %d",
+                                 std::chrono::duration_cast<std::chrono::milliseconds>(
+                                         redraw_end - redraw_start).count(),
+                                 currentFrame.load(std::memory_order_relaxed));
+                Logger::logError(100, "FULL PROCEDURE: %d, FRAME %d",
+                                 std::chrono::duration_cast<std::chrono::milliseconds>(
+                                         redraw_end - startProcess).count(),
+                                 currentFrame.load(std::memory_order_relaxed));
+            }
+        }
+
+    }
+}
+
+void wrappers::WorkersQueue::run4()
+{
+    // OUTPUT WITH ARTIFACTS BUT FAST
+    // RUN2 BUT PARALLEL CONVERT AND ROTATION
+    AHardwareBuffer_Desc desc{};
+    desc.width = 1920;
+    desc.height = 1080;
+    desc.layers = 1;
+    desc.usage = AHARDWAREBUFFER_USAGE_GPU_FRAMEBUFFER;
+    desc.format = AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM;
+
+    AHardwareBuffer * hwb = nullptr;
+    AHardwareBuffer_allocate(&desc, &hwb);
+    std::unique_ptr<AHardwareBuffer, void(*)(AHardwareBuffer *)> localBuffer{hwb, AHardwareBuffer_release};
+
+    constexpr auto slavecount = 3;
+    std::thread slaves[slavecount];
+
+    while (!stop)
+    {
+        std::unique_lock<std::mutex> lockGuard(mQueueProtector);
+        if (!mTasks.empty())
+        {
+            auto task = mTasks.front();
+            mTasks.pop_front();
+            lockGuard.unlock();
+
+            auto startProcess = std::chrono::high_resolution_clock::now();
+
+            // LOCK SOURCE IMAGE
+            HardwareBuffer imageBuffer{};
+            task.image.getHardwareBuffer(imageBuffer);
+            imageBuffer.acquireAndLock();
+
+            // GET YUV PLANES INFO
+            uint8_t * y = nullptr;
+            int32_t y_count = 0;
+            uint8_t * u = nullptr;
+            int32_t u_count = 0;
+            uint8_t * v = nullptr;
+            int32_t v_count = 0;
+
+            task.image.getPlaneData(0, &y, &y_count);
+            task.image.getPlaneData(2, &u, &u_count);
+            task.image.getPlaneData(1, &v, &v_count);
+
+            int32_t y_stride = 0;
+            Logger::ms(AImage_getPlaneRowStride(task.image.handle.get(), 0, &y_stride));
+
+            int32_t u_stride = 0;
+            Logger::ms(AImage_getPlaneRowStride(task.image.handle.get(), 2, &u_stride));
+
+            int32_t v_stride = 0;
+            Logger::ms(AImage_getPlaneRowStride(task.image.handle.get(), 1, &v_stride));
+
+            // GET HARDWARE BUFFER DESCRIPTION
+            auto imageBufferDesc = imageBuffer.describe();
+
+
+            // IN-PLACE SCALE-DOWN
+            //libyuv::NV12Scale(y, y_stride, u, y_stride, 3200, 1800, y, 1920, u, 1920, 1920,1080, libyuv::kFilterBox);
+
+            auto scale_start = std::chrono::high_resolution_clock::now();
+
+            libyuv::ScalePlane(y, y_stride, 3840, 2160, y, 1920, 1920, 1080, libyuv::kFilterBox);
+            libyuv::UVScale(u, y_stride, 3840 / 2, 2160 / 2, u, 1920, 1920 / 2, 1080 / 2, libyuv::kFilterBox);
+
+            auto scale_end = std::chrono::high_resolution_clock::now();
+
+            // LOCK TEMPORARY BUFFER
+            uint8_t * bufferRaw = nullptr;
+            ARect rect2{0, 0, 1920, 1080};
+
+            AHardwareBuffer_lock(localBuffer.get(), AHARDWAREBUFFER_USAGE_CPU_WRITE_OFTEN, -1,
+                                 &rect2, (void**)&bufferRaw);
+
+            AHardwareBuffer_Desc hwbdesc;
+            AHardwareBuffer_describe(localBuffer.get(), &hwbdesc);
+            auto rotate_start = std::chrono::high_resolution_clock::now();
+
+            auto yout = bufferRaw;
+            auto uout = bufferRaw + 1920 * 1080 * 4;
+            auto vout = bufferRaw + 1920 * 1080 * 8;
+            auto uvout = bufferRaw + 1920 * 1080 * 10;
+
+
+            libyuv::RotatePlane90(y, 1920, yout, 1080, 1920, 1080);
+
+            libyuv::RotateUV90(u,
+                               1920,
+                               uout,
+                               1080 / 2,
+                               vout,
+                               1080 / 2,
+                               1920 / 2,
+                               1080 / 2);
+
+            libyuv::MergeUVPlane(uout,
+                                 1080 / 2,
+                                 vout,
+                                 1080 / 2,
+                                 uvout,
+                                 1080,
+                                 1080 / 2,
+                                 1920 / 2);
+            auto rotate_end = std::chrono::high_resolution_clock::now();
+
+            constexpr auto yhstep = 1920 / slavecount;
+            constexpr auto uvhstep = yhstep / 2;
+
+            auto argb_start = std::chrono::high_resolution_clock::now();
+            libyuv::NV12ToARGBMatrix(yout,
+                                             1080,
+                                             uvout,
+                                             1080,
+                                             y,
+                                             1080 * 4,
+                                             &libyuv::kYuvV2020Constants,
+                                             1080,
+                                             1920);
+            auto argb_end = std::chrono::high_resolution_clock::now();
+
+            AHardwareBuffer_unlock(localBuffer.get(), nullptr);
+
+            if (currentFrame > task.frameNumber)
+            {
+                Logger::logInfo(32, "TRYING TO DRAW OLDER FRAME");
+                continue;
+            }
+
+            bool wannause = false;
+            if (surfaceUsed.compare_exchange_strong(wannause, true)) {
+                // LOCK PREVIEW SURFACE
+                ANativeWindow_Buffer surfaceBuffer{};
+                ARect rect{0, 0, 1080, 1920};
+                auto isSurfaceLockFailed = ANativeWindow_lock(task.surface, &surfaceBuffer, &rect);
+
+                auto redraw_start = std::chrono::high_resolution_clock::now();
+                auto out = (uint8_t *) surfaceBuffer.bits;
+
+                libyuv::ARGBCopy(y, 1080 * 4, out, surfaceBuffer.stride * 4,
+                                 surfaceBuffer.width, surfaceBuffer.height);
+
+                auto redraw_end = std::chrono::high_resolution_clock::now();
+
+                isSurfaceLockFailed = ANativeWindow_unlockAndPost(task.surface);
+                currentFrame = task.frameNumber;
+                surfaceUsed = false;
+                Logger::logError(100, "TIME TO SCALE-DOWN: %d, FRAME %d",
+                                 std::chrono::duration_cast<std::chrono::milliseconds>(
+                                         scale_end - scale_start).count(),
+                                 currentFrame.load(std::memory_order_relaxed));
+                Logger::logError(100, "TIME TO MAKE ARGB: %d, FRAME %d",
+                                 std::chrono::duration_cast<std::chrono::milliseconds>(
+                                         argb_end - argb_start).count(),
+                                 currentFrame.load(std::memory_order_relaxed));
+                Logger::logError(100, "TIME TO ROTATE: %d, FRAME %d",
+                                 std::chrono::duration_cast<std::chrono::milliseconds>(
+                                         rotate_end - rotate_start).count(),
+                                 currentFrame.load(std::memory_order_relaxed));
+                Logger::logError(100, "TIME TO REDRAW: %d, FRAME %d",
+                                 std::chrono::duration_cast<std::chrono::milliseconds>(
+                                         redraw_end - redraw_start).count(),
+                                 currentFrame.load(std::memory_order_relaxed));
+                Logger::logError(100, "FULL PROCEDURE: %d, FRAME %d",
+                                 std::chrono::duration_cast<std::chrono::milliseconds>(
+                                         redraw_end - startProcess).count(),
+                                 currentFrame.load(std::memory_order_relaxed));
+            }
+        }
+
+    }
+}
+
+void wrappers::WorkersQueue::run5()
+{
+    // OUTPUT WITH ARTIFACTS BUT FAST
+    // RUN2 BUT PARALLEL CONVERT AND ROTATION
+    AHardwareBuffer_Desc desc{1920, 1080, 1, AHARDWAREBUFFER_USAGE_CPU_WRITE_OFTEN, AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM};
+    HardwareBuffer localBuffer{desc};
+
+    constexpr auto slavecount = 3;
+    std::thread slaves[slavecount];
+
+    while (!stop)
+    {
+        std::unique_lock<std::mutex> lockGuard(mQueueProtector);
+        if (!mTasks.empty())
+        {
+            auto task = mTasks.front();
+            mTasks.pop_front();
+            lockGuard.unlock();
+
+            auto startProcess = std::chrono::high_resolution_clock::now();
+
+            // LOCK SOURCE IMAGE
+            HardwareBuffer imageBuffer{};
+            task.image.getHardwareBuffer(imageBuffer);
+            imageBuffer.acquireAndLock();
+
+            // GET YUV PLANES INFO
+            uint8_t * y = nullptr;
+            int32_t y_count = 0;
+            uint8_t * u = nullptr;
+            int32_t u_count = 0;
+            uint8_t * v = nullptr;
+            int32_t v_count = 0;
+
+            task.image.getPlaneData(0, &y, &y_count);
+            task.image.getPlaneData(2, &u, &u_count);
+            task.image.getPlaneData(1, &v, &v_count);
+
+            int32_t y_stride = 0;
+            task.image.getPlaneRowStride(0, &y_stride);
+
+            int32_t u_stride = 0;
+            task.image.getPlaneRowStride(2, &u_stride);
+
+            int32_t v_stride = 0;
+            task.image.getPlaneRowStride(1, &v_stride);
+
+            // GET HARDWARE BUFFER DESCRIPTION
+            auto imageBufferDesc = imageBuffer.describe();
+
+
+            // IN-PLACE SCALE-DOWN
+            //libyuv::NV12Scale(y, y_stride, u, y_stride, 3200, 1800, y, 1920, u, 1920, 1920,1080, libyuv::kFilterBox);
+
+            auto scale_start = std::chrono::high_resolution_clock::now();
+
+            libyuv::ScalePlane(y, y_stride, 3840, 2160, y, 1920, 1920, 1080, libyuv::kFilterBox);
+            libyuv::UVScale(u, y_stride, 3840 / 2, 2160 / 2, u, 1920, 1920 / 2, 1080 / 2, libyuv::kFilterBox);
+
+            auto scale_end = std::chrono::high_resolution_clock::now();
+
+            // LOCK TEMPORARY BUFFER
+            uint8_t * bufferRaw = nullptr;
+            ARect rect2{0, 0, 1920, 1080};
+
+            AHardwareBuffer_lock(localBuffer.handle, AHARDWAREBUFFER_USAGE_CPU_WRITE_OFTEN, -1,
+                                 &rect2, (void**)&bufferRaw);
+
+            AHardwareBuffer_Desc hwbdesc;
+            AHardwareBuffer_describe(localBuffer.handle, &hwbdesc);
+
+            auto rotate_start = std::chrono::high_resolution_clock::now();
+
+            auto yout = y + 1920 * 1080 * 3;
+            auto uout = y + 1920 * 1080 * 4;
+            auto vout = y + 1920 * 1080 * 5;
+            auto uvout = u;
+
+
+            libyuv::RotatePlane90(y, 1920, yout, 1080, 1920, 1080);
+
+            libyuv::RotateUV90(u,
+                               1920,
+                               uout,
+                               1080 / 2,
+                               vout,
+                               1080 / 2,
+                               1920 / 2,
+                               1080 / 2);
+
+            libyuv::MergeUVPlane(uout,
+                                 1080 / 2,
+                                 vout,
+                                 1080 / 2,
+                                 uvout,
+                                 1080,
+                                 1080 / 2,
+                                 1920 / 2);
+            auto rotate_end = std::chrono::high_resolution_clock::now();
+
+            constexpr auto yhstep = 1920 / slavecount;
+            constexpr auto uvhstep = yhstep / 2;
+
+            auto argb_start = std::chrono::high_resolution_clock::now();
+            libyuv::NV12ToARGBMatrix(yout,
+                                     1080,
+                                     uvout,
+                                     1080,
+                                     y,
+                                     1080 * 4,
+                                     &libyuv::kYuvV2020Constants,
+                                     1080,
+                                     1920);
+            auto argb_end = std::chrono::high_resolution_clock::now();
+
+            AHardwareBuffer_unlock(localBuffer.handle, nullptr);
+
+            if (currentFrame > task.frameNumber)
+            {
+                Logger::logInfo(32, "TRYING TO DRAW OLDER FRAME");
+                continue;
+            }
+
+            bool wannause = false;
+            if (surfaceUsed.compare_exchange_strong(wannause, true)) {
+                // LOCK PREVIEW SURFACE
+                ANativeWindow_Buffer surfaceBuffer{};
+                ARect rect{0, 0, 1080, 1920};
+                auto isSurfaceLockFailed = ANativeWindow_lock(task.surface, &surfaceBuffer, &rect);
+
+                auto redraw_start = std::chrono::high_resolution_clock::now();
+                auto out = (uint8_t *) surfaceBuffer.bits;
+
+                libyuv::ARGBCopy(y, 1080 * 4, out, surfaceBuffer.stride * 4,
                                  surfaceBuffer.width, surfaceBuffer.height);
 
                 auto redraw_end = std::chrono::high_resolution_clock::now();
